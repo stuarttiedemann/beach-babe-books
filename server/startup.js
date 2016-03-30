60f62cd780a36188d0763c2202d569e86ec1f418
stuarttiedemann <stuarttiedemann@yahoo.com>
2016-03-30 14:06:23 -0500

    more email

--- server/startup.js
+++ server/startup.js
@@ -1,3 +1,3 @@
 Meteor.startup( function() {
-  process.env.MAIL_URL = "smtp://postmaster@app4a3a0bdb8e0e4687a85584032f346ece.mailgun.org:c615c4ba94825193afd424c6cf27fbe9@smtp.mailgun.org:587";
+  process.env.MAIL_URL = "smtp://postmaster%404a3a0bdb8e0e4687a85584032f346ece.mailgun.org:c615c4ba94825193afd424c6cf27fbe9@smtp.mailgun.org:587";
 });
